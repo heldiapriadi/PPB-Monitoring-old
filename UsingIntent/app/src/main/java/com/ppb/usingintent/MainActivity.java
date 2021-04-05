5ea22183ef3e2563721dc527f14e95a0268e3691
Heldi Apriadi <heldiapriadi@gmail.com>
2021-04-05 14:57:30 +0700

    Latihan Pertemuan 4

---
+++ UsingIntent/app/src/main/java/com/ppb/usingintent/MainActivity.java
@@ -0,0 +1,31 @@
+package com.ppb.usingintent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    int request_Code = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+        startActivityForResult(new Intent("com.ppb.usingintent.SecondActivity"),request_Code);
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == request_Code) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, data.getData().toString(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+}
